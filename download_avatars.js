ddf3f70a7c2ba5d46736caaeda26f8014b706ce7
DevYves <yfreypons@gmail.com>
2017-11-07 06:08:26 +0000

    improved spacing

--- download_avatars.js
+++ download_avatars.js
@@ -4,6 +4,7 @@ var secret = require('./secret');
 var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
 //function that defines the connection
 function getRepoContributors(repoOwner, repoName, cb) {
   console.log(secret);
@@ -14,11 +15,13 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'Authorization': 'token ' + secret.GITHUB_TOKEN
     }
   };
+
   //used to parse the body object downloaded from github
   request(options, (err, res, body)=> {
     cb(err, JSON.parse(body));
   });
 }
+
 // introduce user input on the command line with argv.
 // if falsey statement used as a guard if user does not input 2 arguments
 getRepoContributors(process.argv[2], process.argv[3], (err, result)=> {
