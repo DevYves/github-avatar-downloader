c16ae642c439e4ebd2630e25c8164dfa1a396a2d
DevYves <yfreypons@gmail.com>
2017-11-07 03:27:56 +0000

    added for loop and parse

--- download_avatars.js
+++ download_avatars.js
@@ -1,26 +1,28 @@
-
 var request = require('request');
-var secret = require('./secret.js');
+var secret = require('./secret');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  console.log(secret);
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization' : 'token ' + secret.GITHUB_TOKEN
-}
+      'Authorization': 'token ' + secret.GITHUB_TOKEN
+    }
   };
 
-
-
   request(options, function(err, res, body) {
-    cb(err, body);
+    cb(err, JSON.parse(body));
   });
 }
 
- getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
+
+  result.forEach(function(contributor) {
+    console.log(contributor.avatar_url);
+  });
 });
