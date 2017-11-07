004a9a412472b6545b76743aa0e8f99a224c4faf
DevYves <yfreypons@gmail.com>
2017-11-07 04:58:13 +0000

    added functionality to download avatar .jpgs

--- download_avatars.js
+++ download_avatars.js
@@ -6,7 +6,7 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   console.log(secret);
-  var options = {
+  const options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
@@ -14,46 +14,30 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
-  request(options, function(err, res, body) {
+  request(options, (err, res, body)=> {
     cb(err, JSON.parse(body));
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors("jquery", "jquery", (err, result)=> {
   console.log("Errors:", err);
   console.log("Result:", result);
 
-  result.forEach(function(contributor) {
-    console.log(contributor.avatar_url);
+  result.forEach((contributor)=> {
+    console.log(contributor);
+    downloadImageByURL(contributor.avatar_url, `./avatar-photos/${contributor.login}.jpg`);
+
   });
+
 });
 
 
 
+
+
 function downloadImageByURL(url, filepath){
-  console.log(url);
-  request.get(url)
-    .on('error', function (err) {                                   // Note 2
-      throw err;
-    })
-  .on('response', function (response) {                           // Note 3
-    console.log('Response Status Code: ', response.statusCode);
-  })
-  .pipe(fs.createWriteStream(filepath));
+  const stream = fs.createWriteStream(filepath);
+  request({url})
+  .pipe(stream);
   console.log("download complete");
 }
-return downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466" ,"./kvirani.jpg");
-
-
-// }
-
-
-// request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
-//        .on('error', function (err) {                                   // Note 2
-//          throw err;
-//        })
-//        .on('response', function (response) {                           // Note 3
-//          console.log('Response Status Code: ', response.statusCode);
-//        })
-//        .pipe(fs.createWriteStream('./future.jpg'));
-//        console.log("download complete") ;
